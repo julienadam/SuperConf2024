1b12c2f7f5b768fc07c38d8155694c3f764a39a0
Julien Adam <julien.adam@arcodia.fr>
2024-05-27 13:51:30 +0200

    Initial version

---
+++ Controllers/InscriptionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperConf2024.Controllers
+{
+    public class InscriptionController : Controller
+    {
+        // GET: InscriptionController
+        public ActionResult Index()
+        {
+            return Content("Ici le formulaire d'inscription");
+            // return View();
+        }
+
+        // GET: InscriptionController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: InscriptionController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
